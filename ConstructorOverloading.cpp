c52911b51966a52e66ab451fbfccdd1c68843b4f
tanvi <tanvimeer.tops@gmail.com>
2022-09-02 16:54:06 +0530

    2 sep

--- ConstructorOverloading.cpp
+++ ConstructorOverloading.cpp
@@ -1,14 +1,23 @@
-/*constructor overloading:same name different parameter.*/
+/*constructor overloading:same name different parameter.
+Destructor destroys the object.
+in c++ object are destroyed manually.
+objects are stored in stack.(stack work in LIFO)
+So the object which is created first are destroyed last.
+
+*/
+/*copy constructor and destructor
+*/
 #include<iostream>
 using namespace std;
 class ConstructorOverloading
 {
 private:
-    int l,b,h;
+    int l,b,h,vol;
 public:
     void volume();
     ConstructorOverloading();
     ConstructorOverloading(int length,int breadth,int height);
+    ConstructorOverloading(ConstructorOverloading &cc);
     ~ConstructorOverloading();
 };
 
@@ -30,12 +39,21 @@ ConstructorOverloading::ConstructorOverloading(int length,int breadth,int height
      b=breadth;
      h=height;
 }
+ConstructorOverloading::ConstructorOverloading(ConstructorOverloading &cc){
+    cout<<"in copy constructor"<<endl;
+    l=cc.l;
+    b=cc.b;
+    h=cc.h;
+
+}
 void ConstructorOverloading:: volume(){
-    cout<<"volume of box:"<<l*b*h<<endl;
+    vol=l*b*h;
+    cout<<"volume of box:"<<vol<<endl;
 }
 
 ConstructorOverloading::~ConstructorOverloading()
 {
+    cout<<"destructor called for:"<<vol<<endl;
 }
 
 int main(){
@@ -43,4 +61,6 @@ ConstructorOverloading dc;
 dc.volume();
 ConstructorOverloading pc(4,5,6);
 pc.volume();
+ConstructorOverloading cc(dc);
+cc.volume();
 }
